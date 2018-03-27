db542b88f04a80575e8cc9163042c3bb735b1163
Carlos <Carlos@LAPTOP-H78PPNR5>
2018-03-26 23:04:40 -1200

    fixed huge mistake

--- src/exceptions/InvalidIndexException.java
+++ src/exceptions/InvalidIndexException.java
@@ -5,7 +5,7 @@ public class InvalidIndexException extends RuntimeException {
 	public InvalidIndexException() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
 	public InvalidIndexException(String arg0) {
 		super(arg0);
 		// TODO Auto-generated constructor stub
